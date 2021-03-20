a099d5dda50324af56719e139fefed20b72cb53f
Saish Shenoy <saish112@gmail.com>
2021-03-21 00:09:55 +0530

    Replaced Manual Intake of data to json file

--- app/src/main/java/com/example/designofsteelstructures/Design.java
+++ app/src/main/java/com/example/designofsteelstructures/Design.java
@@ -1,168 +1,163 @@
-package com.example.designofsteelstructures;
+    package com.example.designofsteelstructures;
 
-import androidx.annotation.NonNull;
+    public class Design {
+        private final String name;
+        private final int length;
+        private final int width;
+        private final int thickness;
+        private final Double sectionalArea;
+        private final Double weightPerMeter;
+        private final Double Cxx;
+        private final Double Ixx;
+        private final Double Iuu;
+        private final Double Izz;
+        private final Double Rxx;
+        private final Double Ruu;
+        private final Double Rzz;
+        private final Double Zxx;
+        private final Double Ixy;
+        private final Double Cyy;
+        private final Double Exx;
+        private final Double Eyy;
+        private final Double Iyy;
+        private final Double Ryy;
+        private final Double Zyy;
 
-public class Design {
-    private int length;
-    private int width;
-    private int thickness;
-    private Double sectionalArea, weightPerMeter, Cxx, Cyy, Exx, Eyy, Ixx, Iyy, Iuu, Ivv, Rxx, Ryy, Ruu, Rvv, Zxx, Zyy, Ixy, Rzz;
+        public String getName() {
+            return name;
+        }
 
+        public int getLength() {
+            return length;
+        }
 
-    @NonNull
-    @Override
-    public String toString() {
-        return length + " length \n" +
-                        width + " width \n" +
-                        thickness + " thickness \n" +
-                        sectionalArea + " sectionalArea \n" +
-                        weightPerMeter + " weightPerMeter \n" +
-                        Cxx + " Cxx \n" +
-                        Cyy + " Cyy \n" +
-                        Exx + " Exx \n" +
-                        Eyy + " Eyy \n" +
-                        Ixx + " Ixx \n" +
-                        Iyy + " Iyy \n" +
-                        Iuu + " Iuu \n" +
-                        Ivv + " Ivv \n" +
-                        Rxx + " Rxx \n" +
-                        Ryy + " Ryy \n" +
-                        Ruu + " Ruu \n" +
-                        Rvv + " Rvv \n" +
-                        Zxx + " Zxx \n" +
-                        Zyy + " Zyy \n" +
-                        Ixy + " Ixy \n\n\n";
+        public int getWidth() {
+            return width;
+        }
+
+        public int getThickness() {
+            return thickness;
+        }
+
+        public Double getSectionalArea() {
+            return sectionalArea;
+        }
+
+        public Double getWeightPerMeter() {
+            return weightPerMeter;
+        }
+
+        public Double getCxx() {
+            return Cxx;
+        }
+
+        public Double getIxx() {
+            return Ixx;
+        }
+
+        public Double getIuu() {
+            return Iuu;
+        }
+
+        public Double getIzz() {
+            return Izz;
+        }
+
+        public Double getRxx() {
+            return Rxx;
+        }
+
+        public Double getRuu() {
+            return Ruu;
+        }
+
+        public Double getRzz() {
+            return Rzz;
+        }
+
+        public Double getZxx() {
+            return Zxx;
+        }
+
+        public Double getIxy() {
+            return Ixy;
+        }
+
+        public Double getCyy() {
+            return Cyy;
+        }
+
+        public Double getExx() {
+            return Exx;
+        }
+
+        public Double getEyy() {
+            return Eyy;
+        }
+
+        public Double getIyy() {
+            return Iyy;
+        }
+
+        public Double getRyy() {
+            return Ryy;
+        }
+
+        public Double getZyy() {
+            return Zyy;
+        }
+
+        //    Constructor for equal angles
+        public Design(String name, int length, int width, int thickness, double sectionalArea, double weightPerMeter, double Cxx,
+                      double Ixx, double Iuu, double Izz, double Rxx, double Ruu, double Rzz, double Zxx, double Ixy) {
+            this.name = name;
+            this.length = length;
+            this.width = width;
+            this.thickness = thickness;
+            this.sectionalArea = sectionalArea;
+            this.weightPerMeter = weightPerMeter;
+            this.Cxx = Cxx;
+            this.Cyy = Cxx;
+            this.Ixx = Ixx;
+            this.Iuu = Iuu;
+            this.Izz = Izz;
+            this.Rxx = Rxx;
+            this.Ryy = Rxx;
+            this.Ruu = Ruu;
+            this.Rzz = Rzz;
+            this.Zxx = Zxx;
+            this.Zyy = Zxx;
+            this.Iyy = Ixx;
+            this.Ixy = Ixy;
+            this.Exx = 0d;
+            this.Eyy = 0d;
+        }
+
+        //    Constructor for unequal angles
+        public Design(String name, int length, int width, int thickness, double sectionalArea, double weightPerMeter, double Cxx,
+                      double Cyy, double Exx, double Eyy, double Ixx, double Iyy, double Iuu, double Izz, double Rxx, double Ryy,
+                      double Ruu, double Rzz, double Zxx, double Zyy, double Ixy) {
+            this.name = name;
+            this.length = length;
+            this.width = width;
+            this.thickness = thickness;
+            this.sectionalArea = sectionalArea;
+            this.weightPerMeter = weightPerMeter;
+            this.Cxx = Cxx;
+            this.Exx = Exx;
+            this.Ixx = Ixx;
+            this.Iuu = Iuu;
+            this.Izz = Izz;
+            this.Rxx = Rxx;
+            this.Ruu = Ruu;
+            this.Rzz = Rzz;
+            this.Zxx = Zxx;
+            this.Ixy = Ixy;
+            this.Cyy = Cyy;
+            this.Eyy = Eyy;
+            this.Iyy = Iyy;
+            this.Ryy = Ryy;
+            this.Zyy = Zyy;
+
+        }
     }
-
-    public int getLength() {
-        return length;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getThickness() {
-        return thickness;
-    }
-
-    public Double getSectionalArea() {
-        return sectionalArea;
-    }
-
-    public Double getWeightPerMeter() {
-        return weightPerMeter;
-    }
-
-    public Double getCxx() {
-        return Cxx;
-    }
-
-    public Double getCyy() {
-        return Cyy;
-    }
-
-    public Double getExx() {
-        return Exx;
-    }
-
-    public Double getEyy() {
-        return Eyy;
-    }
-
-    public Double getIxx() {
-        return Ixx;
-    }
-
-    public Double getIyy() {
-        return Iyy;
-    }
-
-    public Double getIuu() {
-        return Iuu;
-    }
-
-    public Double getIvv() {
-        return Ivv;
-    }
-
-    public Double getRxx() {
-        return Rxx;
-    }
-
-    public Double getRzz() {
-        return Rzz;
-    }
-
-    public Double getRyy() {
-        return Ryy;
-    }
-
-    public Double getRuu() {
-        return Ruu;
-    }
-
-    public Double getRvv() {
-        return Rvv;
-    }
-
-    public Double getZxx() {
-        return Zxx;
-    }
-
-    public Double getZyy() {
-        return Zyy;
-    }
-
-    public Double getIxy() {
-        return Ixy;
-    }
-
-    public Design(int length, int width, int thickness, Double sectionalArea, Double weightPerMeter, Double cxx, Double cyy, Double exx, Double eyy, Double ixx, Double iyy, Double iuu, Double ivv, Double rxx, Double ryy, Double ruu, Double rvv, Double zxx, Double zyy, Double ixy, Double rzz) {
-        this.length = length;
-        this.width = width;
-        this.thickness = thickness;
-        this.sectionalArea = sectionalArea;
-        this.weightPerMeter = weightPerMeter;
-        Cxx = cxx;
-        Cyy = cyy;
-        Exx = exx;
-        Eyy = eyy;
-        Ixx = ixx;
-        Iyy = iyy;
-        Iuu = iuu;
-        Ivv = ivv;
-        Rxx = rxx;
-        Ryy = ryy;
-        Ruu = ruu;
-        Rzz = rzz;
-        Rvv = rvv;
-        Zxx = zxx;
-        Zyy = zyy;
-        Ixy = ixy;
-    }
-
-    public Design(int length, int width, int thickness, Double sectionalArea, Double weightPerMeter, Double cxx, Double cyy, Double ixx, Double iyy, Double iuu, Double ivv, Double rxx, Double ryy, Double ruu, Double rvv, Double zxx, Double zyy, Double ixy, Double rzz) {
-        this.length = length;
-        this.width = width;
-        this.thickness = thickness;
-        this.sectionalArea = sectionalArea;
-        this.weightPerMeter = weightPerMeter;
-        Cxx = cxx;
-        Cyy = cyy;
-        Exx = 0d;
-        Eyy = 0d;
-        Ixx = ixx;
-        Iyy = iyy;
-        Iuu = iuu;
-        Ivv = ivv;
-        Rxx = rxx;
-        Ryy = ryy;
-        Rzz = rzz;
-        Ruu = ruu;
-        Rvv = rvv;
-        Zxx = zxx;
-        Zyy = zyy;
-        Ixy = ixy;
-    }
-}
